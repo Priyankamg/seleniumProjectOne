423985aa26084b8c5054d80237785540f8082373
Priyanka <priyanka.mg26@gmail.com>
2016-06-20 15:11:36 -0700

    programs of HeadFirstJava

---
+++ src/main/java/com/Priyanka/headFirstJava/Excercise1bB.java
@@ -0,0 +1,16 @@
+package com.Priyanka.headFirstJava;
+
+/**
+ * Created by Priyanka on 6/20/2016.
+ */
+public class Excercise1bB {
+    public static void main(String[] args) {
+        int x = 5;
+        while(x > 1) {
+            x = x -1;
+            if(x < 3) {
+                System.out.println("small x");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/Priyanka/headFirstJava/Exercise2D.java
@@ -0,0 +1,17 @@
+package com.Priyanka.headFirstJava;
+
+/**
+ * Created by Priyanka on 6/20/2016.
+ */
+public class Exercise2D {
+    public static void main(String[] args) {
+        int x =0;
+        int y=0;
+        while(x<5) {
+            x = x + 1;
+            y = y + x;
+            System.out.print(x+""+y+" ");
+            x = x + 1;
+        }
+    }
+}
