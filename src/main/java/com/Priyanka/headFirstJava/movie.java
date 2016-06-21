adfc5b5b95e09cd1d1b1c1f133396cc4aa8c089e
Priyanka <priyanka.mg26@gmail.com>
2016-06-20 17:36:36 -0700

    programs of HeadFirstJava

---
+++ src/main/java/com/Priyanka/headFirstJava/movie.java
@@ -0,0 +1,14 @@
+package com.Priyanka.headFirstJava;
+
+/**
+ * Created by Priyanka on 6/20/2016.
+ */
+public class movie {
+    public String title;
+    public String genre;
+    public int rating;
+
+    public void play() {
+        System.out.println("Playing the movie " + title);
+    }
+}
---
+++ src/test/java/com/Priyanka/headFirstJavaTests/dogTest.java
@@ -0,0 +1,23 @@
+package com.Priyanka.headFirstJavaTests;
+
+import com.Priyanka.headFirstJava.dog;
+
+/**
+ * Created by Priyanka on 6/20/2016.
+ */
+public class dogTest {
+    public static void main(String[] args) {
+        dog d = new dog();
+
+        d.size = 40;
+        d.name = "Tommy";
+        d.breed = "Pomeranian";
+
+        System.out.println("Size of the dog is: " + d.size);
+        System.out.println("Name of the dog is: " + d.name);
+        System.out.println("Breed of the dog is: " + d.breed);
+
+        d.bark();
+    }
+
+}
