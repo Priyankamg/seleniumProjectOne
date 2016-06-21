13dc036ff0ed38268731b3d910b9683e1c79410c
Priyanka <priyanka.mg26@gmail.com>
2016-06-20 19:00:35 -0700

    Guess game

---
+++ src/test/java/com/Priyanka/headFirstJavaTests/GuessGameLauncherTest.java
@@ -0,0 +1,13 @@
+package com.Priyanka.headFirstJavaTests;
+
+import com.Priyanka.headFirstJava.GuessGame;
+
+/**
+ * Created by Priyanka on 6/20/2016.
+ */
+public class GuessGameLauncherTest {
+    public static void main(String[] args) {
+        GuessGame gg = new GuessGame();
+        gg.startGame();
+    }
+}
