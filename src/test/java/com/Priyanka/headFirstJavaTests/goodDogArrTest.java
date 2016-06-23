57a0a7a591a04d8497ff45c7d9759638b7efe4a5
Priyanka <priyanka.mg26@gmail.com>
2016-06-23 16:51:22 -0700

    setter getter arr

---
+++ src/test/java/com/Priyanka/headFirstJavaTests/goodDogArrTest.java
@@ -0,0 +1,32 @@
+package com.Priyanka.headFirstJavaTests;
+
+import com.Priyanka.headFirstJava.goodDog;
+
+/**
+ * Created by Priyanka on 6/23/2016.
+ */
+public class goodDogArrTest {
+    public static void main(String[] args) {
+        goodDog[] pets = new goodDog[5];
+        int i;
+
+        for(i=0; i<pets.length; i++) {
+            pets[i] = new goodDog();
+        }
+
+        pets[0].setSize(100);
+        pets[1].setSize(75);
+        pets[2].setSize(50);
+        pets[3].setSize(25);
+        pets[4].setSize(10);
+
+        for(i=0; i<pets.length; i++) {
+            System.out.println("Size of pet " + (i+1) + " is " + pets[i].getSize());
+        }
+
+        for(i=0; i<pets.length;i++) {
+            pets[i].bark();
+        }
+
+    }
+}
