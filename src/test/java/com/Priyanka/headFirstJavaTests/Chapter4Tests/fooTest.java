a79f52eb5f9b69783c60b116cb29c5384086467d
Priyanka <priyanka.mg26@gmail.com>
2016-06-23 18:46:08 -0700

    understanding == and equals

--- src/test/java/com/Priyanka/headFirstJavaTests/Chapter4Tests/fooTest.java
+++ src/test/java/com/Priyanka/headFirstJavaTests/Chapter4Tests/fooTest.java
@@ -18,15 +18,37 @@ public class fooTest {
         foo fooObj3 = fooObj1;
 
         if(fooObj1==fooObj2) {
-            System.out.println("Object 1 and Object 2 are referring to the same object");
+            System.out.println("Object 1 and Object 2 are referring to the same object. Checked using == operator");
         }
         if(fooObj2==fooObj3) {
-            System.out.println("Object 2 and Object 3 are referring to the same object");
+            System.out.println("Object 2 and Object 3 are referring to the same object. Checked using == operator");
         }
         if(fooObj3==fooObj1) {
-            System.out.println("Object 3 and Object 1 are referring to the same object");
+            System.out.println("Object 3 and Object 1 are referring to the same object. Checked using == operator");
+        }
+        if(fooObj1.equals(fooObj2)) {
+            System.out.println("Object 1 and Object 2 are referring to the same object. Checked using equals function");
+        }
+        if(fooObj2.equals(fooObj3)) {
+            System.out.println("Object 2 and Object 3 are referring to the same object. Checked using equals function");
+        }
+        if(fooObj3.equals(fooObj1)) {
+            System.out.println("Object 3 and Object 1 are referring to the same object. Checked using equals function");
         }
 
+        String name1 = "Ria";
+        String name2 = "Ria";
 
+        if(name1==name2) {
+            System.out.println("Name 1 and 2 are the same. Checked using == operator");
+        } else {
+            System.out.println("Name 1 and 2 are different");
+        }
+
+        if(name1.equals(name2)) {
+            System.out.println("Name 1 and 2 are the same. Checked using equals function");
+        } else {
+            System.out.println("Name 1 and 2 are different when checked using equals function");
+        }
     }
 }
