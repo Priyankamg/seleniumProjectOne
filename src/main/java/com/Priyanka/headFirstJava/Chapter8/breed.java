2ef5983bf787335bcc852131fd53f5f60410a16a
Priyanka <priyanka.mg26@gmail.com>
2016-07-09 18:29:37 -0700

    interfaces head first java

--- src/main/java/com/Priyanka/headFirstJava/Chapter8/breed.java
+++ src/main/java/com/Priyanka/headFirstJava/Chapter8/breed.java
@@ -14,7 +14,13 @@ public interface breed {
     there is no ambiguity as to which beFriendly needs to be used
     because the implementation of this method is in the class that
     implements this interface. Since the method definition is in the
-    class that implements it, this will not cause deadly diamond of death
+    class that implements it, this will not cause deadly diamond of death.
+    What is deadly diamond of death?
+    If a class extends 2 classes which have the same methods with same arguments and return types
+    and if these methods are not abstract, which method will the sub-class inherit?
+    There is ambiguity. In order to avoid that, we have interfaces in place.
+    We can have a class implement multiple interfaces since all the methods in the interface
+    are public and abstract by default.
      */
     void beFriendly();
 }
