4f0a63c7de950264f23f82d2b5ad23baf3ef7396
Priyanka <priyanka.mg26@gmail.com>
2016-07-14 11:02:20 -0700

    hackerrank

---
+++ src/test/java/com/Priyanka/hackerrank/first.java
@@ -0,0 +1,17 @@
+package com.Priyanka.hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * Created by Priyanka on 7/14/2016.
+ */
+public class first {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please type in a statement: ");
+        String inputFromUser = scan.nextLine();
+        scan.close();
+        System.out.println("Hello world!");
+        System.out.println(inputFromUser);
+    }
+}
