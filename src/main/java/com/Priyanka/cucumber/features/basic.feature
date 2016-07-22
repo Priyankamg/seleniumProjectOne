f9f915e3fb7ec13f90cab01e03d48145a0830ef9
Priyanka <priyanka.mg26@gmail.com>
2016-07-21 23:56:50 -0700

    data tables cucumber

--- src/main/java/com/Priyanka/cucumber/features/basic.feature
+++ src/main/java/com/Priyanka/cucumber/features/basic.feature
@@ -10,3 +10,18 @@ Feature: Basic tests
     Then I check that I am on "Maybelline The Colossal Kajal Eyeliner Pencil - Black" Page
     And I close the browser
 
+  Scenario: Fill in the shipping address
+    Given I navigate to the website
+    When I search for "bathroom freshner"
+    And I click on "Citrus Magic Solid Air Freshener, Fresh Citrus, 8-Ounce"
+    Then I check that I am on "Citrus Magic Solid Air Freshener, Fresh Citrus, 8-Ounce" Page
+    Then I choose quantity as "20"
+    Then I click on "Add to Shopping Cart" button
+    Then I check that I am on "Added to Cart" Page
+    Then I click on "Proceed to checkout"
+    Then I enter Sign in credentials
+    | email    | priyankamg.test@gmail.com |
+    | password | karu5%99                  |
+    And I close the browser
+
+
--- src/main/java/com/Priyanka/cucumber/features/stepDefinitions.java
+++ src/main/java/com/Priyanka/cucumber/features/stepDefinitions.java
@@ -1,6 +1,7 @@
 package com.Priyanka.cucumber.features;
 
 import com.Priyanka.util.propertyManager;
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -9,7 +10,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -43,19 +47,42 @@ public class stepDefinitions {
 
     @When("^I click on \"([^\"]*)\"$")
     public void clickOnAProduct(String particularProduct) throws Throwable {
-        driver.findElement(By.linkText(particularProduct)).click();
-        WebDriverWait wait = new WebDriverWait(driver,5);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("productTitle")));
+        driver.findElement(By.partialLinkText(particularProduct)).click();
     }
 
     @Then("^I check that I am on \"([^\"]*)\" Page$")
     public void checkWhetherIAmOnTheRightPage(String particularProductPage) throws Throwable {
-        assertThat(driver.findElement(By.id("productTitle")).getText(),is(equalTo(particularProductPage)));
-
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        if(particularProductPage.equals("Added to Cart")) {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("huc-v2-order-row-confirm-text")));
+            assertThat(driver.findElement(By.id("huc-v2-order-row-confirm-text")).getText(),is(equalTo(particularProductPage)));
+        } else {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("productTitle")));
+            assertThat(driver.findElement(By.id("productTitle")).getText(), is(equalTo(particularProductPage)));
+        }
     }
 
     @And("^I close the browser$")
     public void closeTheBrowser() throws Throwable {
         driver.close();
+    }
+
+    @And("^I click on \"([^\"]*)\" button$")
+    public void iClickOnButton(String buttonToClick) throws Throwable {
+        driver.findElement(By.cssSelector("[title='"+buttonToClick+"']")).click();
+    }
+
+    @Then("^I choose quantity as \"([^\"]*)\"$")
+    public void iChooseQuantityAs(String quantity) throws Throwable {
+        Select dropDown = new Select(driver.findElement(By.name("quantity")));
+        dropDown.selectByValue(quantity);
+    }
+
+    @Then("^I enter Sign in credentials$")
+    public void iEnterSignInCredentials(DataTable table) throws Throwable {
+        List<List<String>> signInCred = table.raw();
+        driver.findElement(By.name("email")).sendKeys(signInCred.get(0).get(1));
+        driver.findElement(By.name("password")).sendKeys(signInCred.get(1).get(1));
+        driver.findElement(By.id("signInSubmit")).click();
     }
 }
