091d1f0646ba81496b6d5115207907061757f1ea
Priyanka <priyanka.mg26@gmail.com>
2016-07-28 00:20:21 -0700

    reading a json

---
+++ src/main/java/com/Priyanka/json/address.java
@@ -0,0 +1,43 @@
+package com.Priyanka.json;
+
+/**
+ * Created by Priyanka on 7/27/2016.
+ */
+public class address {
+    private String streetAddress;
+    private String city;
+    private String state;
+    private int postalCode;
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+}
---
+++ src/main/java/com/Priyanka/json/nameAge.java
@@ -0,0 +1,34 @@
+package com.Priyanka.json;
+
+/**
+ * Created by Priyanka on 7/27/2016.
+ */
+public class nameAge {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
