0116cc8f145d88412241d0cd7491f2ce4fc3f03f
Priyanka <priyanka.mg26@gmail.com>
2016-08-09 18:06:54 -0700

    rest api

--- src/test/java/com/Priyanka/jsonTests/mapsApiTest.java
+++ src/test/java/com/Priyanka/jsonTests/mapsApiTest.java
@@ -65,8 +65,14 @@ public class mapsApiTest {
                 .extract().response();
         Assert.assertEquals(response.path("status"),"OK");
         Assert.assertEquals(response.path("results[0].formatted_address"),"1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA");
+        Assert.assertEquals(response.path("results[0].address_components[0].short_name"),"1600");
+        Assert.assertEquals(response.path("results[0].address_components[1].short_name"),"Amphitheatre Pkwy");
+        Assert.assertEquals(response.path("results[0].address_components[2].short_name"),"Mountain View");
+        Assert.assertEquals(response.path("results[0].address_components[3].short_name"),"Santa Clara County");
+        Assert.assertEquals(response.path("results[0].address_components[4].long_name"),"California");
+        Assert.assertEquals(response.path("results[0].address_components[5].long_name"),"United States");
+        Assert.assertEquals(response.path("results[0].address_components[6].types[0]"),"postal_code");
 
-//        System.out.println("Formatted address is: " + response.path("results.formatted_address"));
     }
 
 }
