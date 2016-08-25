c2e80448ce8c46295cecda9bc2f6fa5e8c68d12d
Priyanka <priyanka.mg26@gmail.com>
2016-08-25 13:33:24 -0700

    jenkins run

--- src/test/java/com/Priyanka/jsonTests/jacksonApiTest.java
+++ src/test/java/com/Priyanka/jsonTests/jacksonApiTest.java
@@ -16,7 +16,7 @@ public class jacksonApiTest {
     @Test
     public void testAPIAttributes() {
         ObjectMapper objectMapper  = new ObjectMapper();
-        File simpleDetailsJson = new File("src\\main\\java\\com\\Priyanka\\json\\simpleDetails.json");
+        File simpleDetailsJson = new File("src/main/java/com/Priyanka/json/simpleDetails.json");
         nameAge nameAgeObj=null;
         try {
             nameAgeObj = objectMapper.readValue(simpleDetailsJson,nameAge.class);
@@ -27,14 +27,14 @@ public class jacksonApiTest {
         System.out.println(nameAgeObj.getFirstName());
         System.out.println(nameAgeObj.getLastName());
         System.out.println(nameAgeObj.getAge());
-        System.out.println(nameAgeObj.getAddress().getStreetAddress());
-        System.out.println(nameAgeObj.getAddress().getCity());
-        System.out.println(nameAgeObj.getAddress().getState());
-        System.out.println(nameAgeObj.getAddress().getPostalCode());
-        System.out.println(nameAgeObj.getPhNo().get(0).getNumber());
-        System.out.println(nameAgeObj.getPhNo().get(0).getType());
-        System.out.println(nameAgeObj.getPhNo().get(1).getNumber());
-        System.out.println(nameAgeObj.getPhNo().get(1).getType());
+//        System.out.println(nameAgeObj.getAddress().getStreetAddress());
+//        System.out.println(nameAgeObj.getAddress().getCity());
+//        System.out.println(nameAgeObj.getAddress().getState());
+//        System.out.println(nameAgeObj.getAddress().getPostalCode());
+//        System.out.println(nameAgeObj.getPhNo().get(0).getNumber());
+//        System.out.println(nameAgeObj.getPhNo().get(0).getType());
+//        System.out.println(nameAgeObj.getPhNo().get(1).getNumber());
+//        System.out.println(nameAgeObj.getPhNo().get(1).getType());
 
         assertEquals("Age is incorrect", nameAgeObj.getAge(),26);
     }
--- src/test/java/com/Priyanka/jsonTests/simpleDetailsTest.java
+++ src/test/java/com/Priyanka/jsonTests/simpleDetailsTest.java
@@ -1,21 +1,27 @@
 package com.Priyanka.jsonTests;
 
+import com.Priyanka.json.nameAge;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Priyanka on 7/28/2016.
  */
 public class simpleDetailsTest {
     public static void main(String[] args) {
-//        nameAge  na = null;
-//        File simpleDetailsJson =  new File("src/main/java/com/Priyanka/json/simpleDetails.json");
-//        ObjectMapper omp = new ObjectMapper();
-//        try {
-//            na = omp.readValue(simpleDetailsJson,nameAge.class);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(na.getFirstName());
-//        System.out.println(na.getLastName());
-//        System.out.println(na.getAge());
+        nameAge na = null;
+        File simpleDetailsJson =  new File("src/main/java/com/Priyanka/json/simpleDetails.json");
+        ObjectMapper omp = new ObjectMapper();
+        try {
+            na = omp.readValue(simpleDetailsJson,nameAge.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(na.getFirstName());
+        System.out.println(na.getLastName());
+        System.out.println(na.getAge());
 //        System.out.println(na.getAddress().getStreetAddress());
 //        System.out.println(na.getAddress().getCity());
 //        System.out.println(na.getAddress().getState());
