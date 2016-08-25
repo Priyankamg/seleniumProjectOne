15b837a72a978a43a462ff668ca6a66786313831
Priyanka <priyanka.mg26@gmail.com>
2016-08-25 13:29:52 -0700

    jenkins run

--- src/test/java/com/Priyanka/jsonTests/simpleDetailsTest.java
+++ src/test/java/com/Priyanka/jsonTests/simpleDetailsTest.java
@@ -1,34 +1,28 @@
 package com.Priyanka.jsonTests;
 
-import com.Priyanka.json.nameAge;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
-
 /**
  * Created by Priyanka on 7/28/2016.
  */
 public class simpleDetailsTest {
     public static void main(String[] args) {
-        nameAge  na = null;
-        File simpleDetailsJson =  new File("src/main/java/com/Priyanka/json/simpleDetails.json");
-        ObjectMapper omp = new ObjectMapper();
-        try {
-            na = omp.readValue(simpleDetailsJson,nameAge.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(na.getFirstName());
-        System.out.println(na.getLastName());
-        System.out.println(na.getAge());
-        System.out.println(na.getAddress().getStreetAddress());
-        System.out.println(na.getAddress().getCity());
-        System.out.println(na.getAddress().getState());
-        System.out.println(na.getAddress().getPostalCode());
-        System.out.println(na.getPhNo().get(0).getType());
-        System.out.println(na.getPhNo().get(0).getNumber());
-        System.out.println(na.getPhNo().get(1).getType());
-        System.out.println(na.getPhNo().get(1).getNumber());
+//        nameAge  na = null;
+//        File simpleDetailsJson =  new File("src/main/java/com/Priyanka/json/simpleDetails.json");
+//        ObjectMapper omp = new ObjectMapper();
+//        try {
+//            na = omp.readValue(simpleDetailsJson,nameAge.class);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(na.getFirstName());
+//        System.out.println(na.getLastName());
+//        System.out.println(na.getAge());
+//        System.out.println(na.getAddress().getStreetAddress());
+//        System.out.println(na.getAddress().getCity());
+//        System.out.println(na.getAddress().getState());
+//        System.out.println(na.getAddress().getPostalCode());
+//        System.out.println(na.getPhNo().get(0).getType());
+//        System.out.println(na.getPhNo().get(0).getNumber());
+//        System.out.println(na.getPhNo().get(1).getType());
+//        System.out.println(na.getPhNo().get(1).getNumber());
     }
 }
