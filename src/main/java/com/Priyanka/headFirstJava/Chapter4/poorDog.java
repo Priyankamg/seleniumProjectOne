e04ce878a23bbc4c2195062150c914c384b59e90
Priyanka <priyanka.mg26@gmail.com>
2016-09-01 14:06:29 -0700

    adding comment

--- src/main/java/com/Priyanka/headFirstJava/Chapter4/poorDog.java
+++ src/main/java/com/Priyanka/headFirstJava/Chapter4/poorDog.java
@@ -4,6 +4,8 @@ package com.Priyanka.headFirstJava.Chapter4;
  * Created by Priyanka on 6/23/2016.
  * This is a class where the instance variables are not assigned any value
  * using the setter methods. What is the DEFAULT VALUE of the instance variables?
+ * Default value of size = 0
+ * Default value of name = null
  */
 public class poorDog {
     private int size;
