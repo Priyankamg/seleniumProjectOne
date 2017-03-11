00599b3ed8ad101963d84ac1d25d03609112176d
Prinks <prinks@pmula-mbpcorpitsoninccom.local>
2017-03-11 13:08:39 -0800

    First commit in 2017

---
+++ src/main/java/com/Priyanka/simplePrograms/testing.java
@@ -0,0 +1,10 @@
+package com.Priyanka.simplePrograms;
+
+/**
+ * Created by prinks on 3/11/17.
+ */
+public class testing {
+    public static void main(String[] args) {
+        System.out.println("Will begin learning new things :)");
+    }
+}
