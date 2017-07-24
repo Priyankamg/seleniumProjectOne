871f5cb12a32f543a3a11f2ffe65b900d9e35b65
Prinks <prinks@pmula-mbpcorpitsoninccom.local>
2017-07-23 22:45:27 -0700

    DSA: Lesson 2

--- src/main/java/com/Priyanka/dsa/numbers.java
+++ src/main/java/com/Priyanka/dsa/numbers.java
@@ -4,17 +4,18 @@ package com.Priyanka.dsa;
  * Created by prinks on 7/23/17.
  */
 public class numbers {
-    int[] nums;
+    int[] tables3;
+    int[] tables2;
     int count;
 
-    public numbers(int[] nums) {
-        this.nums = nums;
+    public numbers(int[] tables3) {
+        this.tables3 = tables3;
     }
 
     public int countEvenNumbers() {
         count = 0;
-        for(int i=0;i<nums.length;i++) {
-            if((nums[i]%2)==0) {
+        for(int i=0;i<tables3.length;i++) {
+            if((tables3[i]%2)==0) {
                 count++;
             }
         }
@@ -23,13 +24,31 @@ public class numbers {
 
     public int countOddNumbers() {
         count = 0;
-        for(int i=0;i<nums.length;i++) {
-            if((nums[i]%2)!=0) {
+        for(int i=0;i<tables3.length;i++) {
+            if((tables3[i]%2)!=0) {
                 count++;
             }
         }
         return count;
     }
 
+    public int getElementFrom(int index) {
+        return tables3[index];
+    }
 
+    public void setTables2(int[] multiplicationOf2) {
+        this.tables2 = multiplicationOf2;
+    }
+
+    public int getNumberOfDuplicates() {
+        count = 0;
+        for(int i=0; i<tables3.length; i++) {
+            for(int j=0; j<tables2.length; j++) {
+                if(tables3[i]==tables2[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
--- src/test/java/com/Priyanka/dsaTest/numbersTest.java
+++ src/test/java/com/Priyanka/dsaTest/numbersTest.java
@@ -7,15 +7,25 @@ import com.Priyanka.dsa.numbers;
  */
 public class numbersTest {
     public static void main(String[] args) {
-        int[] ages = new int[10];
+        int[] multiplicationOf3 = new int[10];
+        int[] multiplicationOf2 = new int[10];
 
         for(int i=0; i<10;i++) {
-            ages[i] = i * 3;
+            multiplicationOf3[i] = (i+1) * 3;
         }
 
-        numbers n = new numbers(ages);
+        for(int i=0; i<10; i++) {
+            multiplicationOf2[i] = (i+1) * 2;
+        }
 
-        System.out.println("Count of even numbers: " + n.countEvenNumbers());
-        System.out.println("Count of odd numbers: " + n.countOddNumbers());
+        numbers n = new numbers(multiplicationOf3);
+        n.setTables2(multiplicationOf2);
+
+        System.out.println("Count of even numbers: " + n.countEvenNumbers()); // Linear operation
+        System.out.println("Count of odd numbers: " + n.countOddNumbers()); // Linear operation
+        System.out.println("The 1st element in the array is: " + n.getElementFrom(0));
+        System.out.println("The 5th element in the array is: " + n.getElementFrom(4));
+
+        System.out.println("Number of duplicates present in tables of 2 and tables of 3 are: " + n.getNumberOfDuplicates());
     }
 }
