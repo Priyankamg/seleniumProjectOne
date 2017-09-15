ef6fe2996068fd8deae3ccf2d712476548dd5bcb
Prinks <prinks@pmula-mbpcorpitsoninccom.local>
2017-09-14 19:42:23 -0700

    Circular Linked List

--- src/main/java/com/Priyanka/dsa/NodeCircularLinkedList.java
+++ src/main/java/com/Priyanka/dsa/NodeCircularLinkedList.java
@@ -44,10 +44,49 @@ public class NodeCircularLinkedList {
 
     public void displayLinkedList() {
         NodeSinglyLinkedList node = first;
-
-        while(node!=last.next) {
-            System.out.println(node + " " + node.data + " " + node.next);
-            node = node.next;
+        if(!isEmpty()) {
+            while (node != last.next) {
+                System.out.println(node + " " + node.data + " " + node.next);
+                node = node.next;
+            }
+        } else {
+            throw new NullPointerException("No elements in the Linked List");
         }
     }
+
+    public int deleteFirst() {
+        NodeSinglyLinkedList temp = first;
+        if(!isEmpty()) {
+            first = first.next;
+            return temp.data;
+        } else {
+            throw new NullPointerException("Inside deleteFirst: No elements in the Linked List");
+        }
+    }
+
+    public int deleteLast() {
+        NodeSinglyLinkedList temp = first;
+
+        System.out.println("First: " + first);
+        System.out.println("Last: " + last);
+        if(!isEmpty()) {
+            int data = last.data;
+            if (!(first == last)) {
+                while (temp.next != last) {
+                    temp = temp.next;
+                }
+                last = temp;
+                last.next = null;
+
+            } else {
+                last = null;
+                first = null;
+            }
+            return data;
+        } else {
+            throw new NullPointerException("Inside DeleteLast: No elements to delete in the Linked List");
+        }
+
+    }
+
 }
--- src/test/java/com/Priyanka/dsaTest/NodeCircularLinkedListTest.java
+++ src/test/java/com/Priyanka/dsaTest/NodeCircularLinkedListTest.java
@@ -14,6 +14,7 @@ public class NodeCircularLinkedListTest {
         l.insertLast(40);
         l.insertLast(50);
         l.displayLinkedList();
+        System.out.println("---------");
 
         NodeCircularLinkedList l2 = new NodeCircularLinkedList();
         l2.insertFirst(5);
@@ -21,6 +22,80 @@ public class NodeCircularLinkedListTest {
         l2.insertFirst(3);
         l2.insertFirst(2);
         l2.insertFirst(1);
-        l2.displayLinkedList();
+        try {
+            l2.displayLinkedList();
+            System.out.println("------");
+
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+            l2.displayLinkedList();
+            System.out.println("------");
+
+
+            System.out.println("Deleting the last element in the linked list: " + l2.deleteLast());
+            l2.displayLinkedList();
+            System.out.println("----");
+
+            System.out.println("Deleting the last element in the linked list: " + l2.deleteLast());
+            l2.displayLinkedList();
+            System.out.println("----");
+
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+            l2.displayLinkedList();
+            System.out.println("------");
+
+            System.out.println("Deleting the last element in the linked list: " + l2.deleteLast());
+
+            System.out.println(">>>");
+            System.out.println("Deleting the last element in the linked list: " + l2.deleteLast());
+            System.out.println(">>>");
+
+//            l2.displayLinkedList();
+//            System.out.println("----");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        l2.insertLast(100);
+        l2.insertLast(200);
+        l2.insertLast(300);
+        l2.insertLast(400);
+        l2.insertLast(500);
+
+        try {
+            l2.displayLinkedList();
+            System.out.println("===");
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+            l2.displayLinkedList();
+            System.out.println("===");
+
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+            l2.displayLinkedList();
+            System.out.println("===");
+
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+            l2.displayLinkedList();
+            System.out.println("===");
+
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+            l2.displayLinkedList();
+            System.out.println("===");
+
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+          //  l2.displayLinkedList();
+            System.out.println("===");
+
+            System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+           // l2.displayLinkedList();
+            System.out.println("===");
+
+        System.out.println("Deleting the first element from the linked list: " + l2.deleteFirst());
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+
+
+
     }
 }
