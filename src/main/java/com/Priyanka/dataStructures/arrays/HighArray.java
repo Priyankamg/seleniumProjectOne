c9bba9dffe14a464f4f0bce8577baf591b6275ae
Prinks <prinks@pmula-mbpcorpitsoninccom.local>
2018-01-02 16:20:27 -0800

    Arrays: Abstraction

---
+++ src/main/java/com/Priyanka/dataStructures/arrays/HighArray.java
@@ -0,0 +1,7 @@
+package com.Priyanka.dataStructures.arrays;
+
+/**
+ * Created by prinks on 1/2/18.
+ */
+public class HighArray {
+}
---
+++ src/test/java/com/Priyanka/dataStructures/arraysTest/HighArrayTest.java
@@ -0,0 +1,7 @@
+package com.Priyanka.dataStructures.arraysTest;
+
+/**
+ * Created by prinks on 1/2/18.
+ */
+public class HighArrayTest {
+}
