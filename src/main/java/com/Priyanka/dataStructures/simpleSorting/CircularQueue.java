324c87c514d53a653c9d51b371dbb74b87fa7db5
Prinks <prinks@pmula-mbpcorpitsoninccom.local>
2018-01-12 17:02:39 -0800

    Circular queue

--- src/main/java/com/Priyanka/dataStructures/simpleSorting/CircularQueue.java
+++ src/main/java/com/Priyanka/dataStructures/simpleSorting/CircularQueue.java
@@ -4,53 +4,74 @@ package com.Priyanka.dataStructures.simpleSorting;
  * Created by prinks on 1/10/18.
  */
 public class CircularQueue {
-    private int[] queueArray;
+    private int[] circularQueueArray;
     private int maxSize;
     private int front;
     private int rear;
-    private int numberOfElements;
+    private int numberElements;
 
     public CircularQueue(int size) {
+        circularQueueArray = new int[size];
         maxSize = size;
-        queueArray = new int[size];
         front = 0;
         rear = -1;
-        numberOfElements = 0;
-    }
-
-    public boolean isFull() {
-        return rear == maxSize-1;
+        numberElements = 0;
     }
 
     public boolean isEmpty() {
-        return front > rear;
+        return numberElements == 0;
     }
 
-    public void insert(int element) {
-        if(isFull()) {
-            rear = -1;
+    public boolean isFull() {
+        return numberElements == maxSize;
+    }
+
+    public void insert(int element) throws Exception {
+        if(!isFull()) {
+                if(rear==maxSize-1 && front>0)
+                    rear = -1;
+
+                if(front==maxSize && rear>=0)
+                    front = 0;
+
+                circularQueueArray[++rear] = element;
+                numberElements++;
+            } else {
+            throw new Exception("Queue is full");
         }
-
-        queueArray[++rear] = element;
-
-        if(!isFull())
-            numberOfElements++;
     }
 
     public int remove() throws Exception {
         if(!isEmpty()) {
-            int element = queueArray[front];
+            if(front==maxSize && rear>=0)
+                front = 0;
+            int element = circularQueueArray[front];
             front++;
-            numberOfElements--;
+            numberElements--;
             return element;
         } else {
-            throw new Exception("Queue is empty.");
+            throw new Exception("Queue is empty");
         }
     }
 
     public void display() {
-        for(int i=front; i<=rear; i++)
-            System.out.print(queueArray[i] + " ");
-        System.out.println();
+        if(front < rear) {
+            for(int i=front; i<=rear; i++) {
+                System.out.print(circularQueueArray[i] + " ");
+            }
+            System.out.println();
+        } else {
+            if(rear>0) {
+                for(int i=0; i<rear; i++)
+                    System.out.print(circularQueueArray[i] + " ");
+            }
+            for(int i=rear; i<front; i++)
+                System.out.print(circularQueueArray[i] + " ");
+
+            for(int i=front; i<=maxSize-1; i++)
+                System.out.print(circularQueueArray[i] + " ");
+            System.out.println();
+        }
+
     }
 }
--- src/test/java/com/Priyanka/dataStructuresTest/simpleSortingTest/CircularQueueTest.java
+++ src/test/java/com/Priyanka/dataStructuresTest/simpleSortingTest/CircularQueueTest.java
@@ -10,19 +10,63 @@ import org.junit.Test;
 public class CircularQueueTest {
     CircularQueue circularQueue;
 
-    @Before
-    public void initialize() {
-        circularQueue = new CircularQueue(5);
-    }
-
     @Test
-    public void testInsert() {
-        for(int i=0; i<5; i++)
-            circularQueue.insert((i+1)*9);
-        circularQueue.display();
+    public void testCircularQueue() {
+        circularQueue = new CircularQueue(5);
+        try {
+            circularQueue.insert(10);
+            circularQueue.insert(20);
+            circularQueue.insert(30);
+            circularQueue.insert(40);
+            circularQueue.insert(50);
+            circularQueue.display();
 
-        circularQueue.insert(10);
+            while (!circularQueue.isEmpty()) {
+                System.out.print(circularQueue.remove() + " ");
+            }
+            System.out.println();
 
-        circularQueue.display();
+            circularQueue.insert(100);
+            circularQueue.insert(200);
+            circularQueue.display();
+
+            System.out.println(circularQueue.remove());
+
+            circularQueue.display();
+
+            circularQueue.insert(300);
+            circularQueue.insert(400);
+            circularQueue.insert(500);
+            circularQueue.insert(600);
+            circularQueue.display();
+
+            while(!circularQueue.isEmpty())
+                System.out.print(circularQueue.remove() + " ");
+            System.out.println();
+
+            circularQueue.insert(3);
+            circularQueue.insert(6);
+            circularQueue.insert(9);
+
+            circularQueue.display();
+
+            circularQueue.insert(12);
+            circularQueue.insert(15);
+            circularQueue.display();
+
+            System.out.println(circularQueue.remove());
+            System.out.println(circularQueue.remove());
+            circularQueue.insert(18);
+            circularQueue.insert(21);
+            circularQueue.display();
+
+            while(!circularQueue.isEmpty())
+                System.out.print(circularQueue.remove() + " ");
+            System.out.println();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
